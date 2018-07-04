ab358110a149e5b4fb2ef4bb298974bd9ceb8bb1
huaweiyang94 <huaweiyang94@gmail.com>
2018-07-03 23:03:36 -0700

    lesson #8

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var indexRouter = require('./routes/index');
 
@@ -23,6 +24,8 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//order is important for validator
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
