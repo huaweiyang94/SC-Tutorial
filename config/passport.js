ada381d65dc99a4bc17a809a84b8a645021ce160
huaweiyang94 <huaweiyang94@gmail.com>
2018-07-04 22:55:07 -0700

    Lesson #9

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
-//The strategy to signup(create) a new user
+//The strategy for signup(create) a new user
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
@@ -43,5 +43,33 @@ passport.use('local.signup', new LocalStrategy({
             }
             return done(null, newUser);
         });
+    });
+}));
+
+//The strategy for signin a user
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function(req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(error => messages.push(error.msg));
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({'email': email}, function(err, user) {
+        if (err) {
+           return done(err);
+        }
+        if (!user) {
+            return done(null, false, {message: 'No user found.'});
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: 'Wrong password.'});
+        }
+        return done(null, user);
     });
 }));
